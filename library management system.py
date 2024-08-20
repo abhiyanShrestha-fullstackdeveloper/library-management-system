5f7c67680d4ceb3e2727448361a978d85bc66fe3
abhiyanShrestha-fullstackdeveloper <abhiyanshrestha328@gmail.com>
2024-08-20 10:22:59 +0545

    add books
    added add books in the system for adding books in the system that is very necessary in the library management system plus i have changed text first letter to captial

--- library management system.py
+++ library management system.py
@@ -12,7 +12,8 @@ def register():
         print(f'{username},{password},{usertype}\n')
         print('Registration Successful')
 
-
+############################################
+# Login
 def login():
     print('Library Management System')
     print('Register')
@@ -27,12 +28,27 @@ def login():
             print('Welcome to Our Library Management System')
         else:
             print('Username or Password is wrong!')
-            
 
+############################################
+# Add Books
+
+def add_books():
+    bookname = input('Enter Book Name: ')
+    bookcategory = input('Enter Book Category: ')
+    bookdescription = input('Enter Book Description: ')
+    bookauthor = input('Enter Book Author Name: ')
+    bookprice = int(input('Enter Book Price: '))
+    
+    with open('bookslist.txt','a') as f:
+        f.write(f'{bookname},{bookcategory},{bookdescription},{bookauthor},{bookprice}\n')
+        print('Book Added Successfully in System')
+            
+###########################################
+# Main Program
 while True:
     print('Library Management System')
     print('Choices')
-    Choice = input('1.register\t\t2.login\t\tq.quit: ')
+    Choice = input('1.Register\t\t2.Login\t\tq.Quit: ')
     
     if Choice == '1':
         register()
@@ -43,8 +59,43 @@ while True:
         if usertype:
             if usertype == 'superadmin':
                 while True:
+                    Choice = input('1.Book List 2.Add Books 3.Book For Rent 4. Delete Book List q.Logout: ')
                     
-            
+                    if Choice == '1':
+                        view_booklist()
+                    
+                    elif Choice == '2':
+                        add_books()
+                    
+                    elif Choice == '3':
+                        book_forrent()
+                    
+                    elif Choice == '4':
+                        delete_booklist()
+                    
+                    elif Choice == 'q':
+                        break
+                    else:
+                        print('Invalid Choice!')
+            else:
+                while True:
+                    Choice == input('1.Book List 2. Add Books 3. Book For Rent q. Logout: ')
+                    
+                    if Choice == '1':
+                        view_booklist()
+                    
+                    elif Choice == '2':
+                        add_books()
+                    
+                    elif Choice == '3':
+                        book_forrent()
+                    
+                    elif Choice == 'q':
+                        break
+                    
+                    else:
+                        print('Invalid Choice!')
+                        
     
     elif Choice == 'q':
         break
